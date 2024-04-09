b37d64b86e9526f6270c2088f5de01abd7d58eb0
Perslay <natalia.44.fedyk@gmail.com>
2024-04-09 23:10:00 +0200

    Render names
    After each successful submit, a new name in list is rendered

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // https://www.npmjs.com/package/nanoid
-// import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 // model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
 
 export class App extends Component {
@@ -9,18 +9,47 @@ export class App extends Component {
     name: '',
   };
 
+  handleChange = event => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  add = event => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const name = form.elements.name.value;
+
+    const updatedContacts = [
+      ...this.state.contacts,
+      {
+        name: name,
+        id: nanoid(),
+      },
+    ];
+
+    this.setState({
+      contacts: updatedContacts,
+    });
+
+    form.reset();
+  };
+
   render() {
+    const { contacts, name } = this.state;
+
     return (
       <>
         <div>
           <p>Phonebook</p>
           <div>
             <p>Name</p>
-            <form action="">
+            <form onSubmit={this.add}>
               <input
                 type="text"
                 name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                pattern="^[a-zA-Zа-яА-Я]+((['\s\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
               />
@@ -31,9 +60,9 @@ export class App extends Component {
         <div>
           <p>Contacts</p>
           <ul>
-            <li>a</li>
-            <li>a</li>
-            <li>a</li>
+            {contacts.map(contact => (
+              <li key={contact.id}>{contact.name}</li>
+            ))}
           </ul>
         </div>
       </>
